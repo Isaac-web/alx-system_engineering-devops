8101bbd3e5e1adda6bda17f95b04fed029bfc79e
Isaac Takiy <kanytakiy@gmail.com>
2022-11-14 23:41:46 +0000

    Modify 0-gatter_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,10 +3,10 @@
 Retrieves an employees data
 with his todo list
 """
+import json
 import requests
 from requests.exceptions import HTTPError
 from sys import argv
-import json
 
 
 def fetch_employee(id=None):
@@ -20,7 +20,7 @@ def fetch_todos(id=None):
     all_todos = requests.get(
         "https://jsonplaceholder.typicode.com/users/{}/todos/".format(id))
     all_todos = list(all_todos.json())
-    
+
     completed = []
     for t in all_todos:
         if(t["completed"]):
@@ -28,11 +28,13 @@ def fetch_todos(id=None):
 
     return all_todos, completed
 
+
 if __name__ == "__main__":
     emp_name = fetch_employee(argv[1])["name"]
     all_todos, completed = fetch_todos(argv[1])
-    
-    print("Employee {} is done with tasks({}/{}):"
+
+    print(
+            "Employee {} is done with tasks({}/{}):"
             .format(emp_name, len(completed), len(all_todos)))
 
     for t in completed:
